539a2608ad8e3afb87591378587201d7b74b358b
catharygr <103951447+catharygr@users.noreply.github.com>
2023-06-15 11:19:44 +0100

    Trabajando en la serverless function en netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  const url =
+    "https://listillo-openai.netlify.app/.netlify/functions/openAiFetch";
+
+  async function getOpenAIData() {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      body: "Hola Sasa",
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+  getOpenAIData();
+  return <h1>Hola mundo</h1>;
+}
 
 export default App;
