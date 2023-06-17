0e0a17db78e56d84153c5ebcdd574a9f802a9601
catharygr <103951447+catharygr@users.noreply.github.com>
2023-06-17 13:05:43 +0100

    Conectados con Netlify

--- netlify/functions/openAiFetch.js
+++ netlify/functions/openAiFetch.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
-import { Configuration, OpenAIApi } from "openai";
 
+import { Configuration, OpenAIApi } from "openai";
 const openAiConf = new Configuration({
   apiKey: process.env.REACT_APP_API_KEY,
 });
-
-const openai = new OpenAIApi(openAiConf);
+const openAI = new OpenAIApi(openAiConf);
 
 exports.handler = async function (event) {
   const headers = {
@@ -22,17 +21,57 @@ exports.handler = async function (event) {
       body: "This was a preflight call!",
     };
   } else if (event.httpMethod === "POST") {
-    const conversationArray = JSON.parse(event.body);
-    const response = await openai.createChatCompletion({
-      model: "gtp-3.5-turbo",
-      messages: conversationArray,
+    const conversationArr = JSON.parse(event.body);
+
+    const response = await openAI.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: conversationArr,
     });
-    const dataDesdeOpenAI = JSON.stringify(response.data.choices[0].message);
+    const dataFromOpenAi = JSON.stringify(response.data.choices[0].message);
 
     return {
       statusCode: 200,
       headers,
-      body: dataDesdeOpenAI,
+      body: dataFromOpenAi,
     };
   }
 };
+
+// /* eslint-disable no-undef */
+// import { Configuration, OpenAIApi } from "openai";
+
+// const openAiConf = new Configuration({
+//   apiKey: process.env.REACT_APP_API_KEY,
+// });
+
+// const openai = new OpenAIApi(openAiConf);
+
+// exports.handler = async function (event) {
+//   const headers = {
+//     "Access-Control-Allow-Origin": "http://localhost:8888",
+//     "Access-Control-Allow-Headers": "Content-Type",
+//     "Access-Control-Allow-Methods": "GET, POST, OPTIONS, DELETE, HEAD,",
+//     "Access-Control-Max-Age": "86400",
+//   };
+
+//   if (event.httpMethod === "OPTIONS") {
+//     return {
+//       statusCode: 200,
+//       headers,
+//       body: "This was a preflight call!",
+//     };
+//   } else if (event.httpMethod === "POST") {
+//     const conversationArray = JSON.parse(event.body);
+//     const response = await openai.createChatCompletion({
+//       model: "gtp-3.5-turbo",
+//       messages: conversationArray,
+//     });
+//     const dataDesdeOpenAI = JSON.stringify(response.data.choices[0].message);
+
+//     return {
+//       statusCode: 200,
+//       headers,
+//       body: dataDesdeOpenAI,
+//     };
+//   }
+// };
